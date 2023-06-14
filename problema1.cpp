f5d6fba84caf4a260eac473837603c59cd5926b6
Jose Luis Lopez Arevalo <123702001+Luar44@users.noreply.github.com>
2023-06-13 22:45:26 -0600

    Add files via upload

---
+++ problema1.cpp
@@ -0,0 +1,41 @@
+//g++ -std=c++17 a.cpp && a < in.txt > out.txt
+#include <bits/stdc++.h>
+#include <cmath>
+#include <cctype>
+#define f first
+#define s second
+#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define pb push_back
+#define all(s) begin(s), end(s)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define sz(s) int(s.size())
+#define ENDL '\n'
+#define IO
+using namespace std;
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+#define deb(x) cout << #x": " << (x) << endl;
+template <typename T> static constexpr T inf = numeric_limits<T>::max() / 2;
+
+vector <int> all_divisors(int a){
+    vector<int>r;
+    for(int i=2;i*i<a:i++){
+        if(a%i==0){
+            
+        }
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int> r(n);
+    for(int i=0;i<n;i++){
+        cin>>r[i];
+
+    }
+    sort(r.begin(),r.end());
+    x=r[0]*r[n-1];
+
+}
